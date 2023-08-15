89f457d2b1d6565cc7997919b3241b0e557336ea
cuiyiming1998 <1269567664@qq.com>
2023-08-15 17:19:16 +0800

    feat: parser

--- __tests__/parser.spec.ts
+++ __tests__/parser.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from 'vitest'
 import { parser } from '../src'
 import { TokenTypes } from '../src/tokenizer'
+import { NodeTypes } from '../src/parser'
 
 test('tokenizer', () => {
   const tokens = [
@@ -16,31 +17,135 @@ test('tokenizer', () => {
   ]
 
   const ast = {
-    type: 'Program',
+    type: NodeTypes.ROOT,
     body: [
       {
-        type: 'CallExpression',
+        type: NodeTypes.CALL_EXPRESSION,
         name: 'add',
         params: [
           {
-            type: 'NumberLiteral',
+            type: NodeTypes.NUMBER,
             value: '2',
           },
           {
-            type: 'CallExpression',
-            name: 'subtract',
+            type: NodeTypes.CALL_EXPRESSION,
+            name: 'substract',
             params: [
               {
-                type: 'NumberLiteral',
+                type: NodeTypes.NUMBER,
                 value: '4',
               },
               {
-                type: 'NumberLiteral',
+                type: NodeTypes.NUMBER,
                 value: '2',
               },
             ],
           },
         ],
+      },
+    ],
+  }
+
+  expect(parser(tokens)).toEqual(ast)
+})
+
+test('simple parser', () => {
+  const tokens = [
+    { type: TokenTypes.PAREN, value: '(' },
+    { type: TokenTypes.NAME, value: 'add' },
+    { type: TokenTypes.NUMBER, value: '2' },
+    { type: TokenTypes.NUMBER, value: '4' },
+    { type: TokenTypes.PAREN, value: ')' },
+  ]
+
+  const ast = {
+    type: NodeTypes.ROOT,
+    body: [
+      {
+        type: NodeTypes.CALL_EXPRESSION,
+        name: 'add',
+        params: [
+          {
+            type: NodeTypes.NUMBER,
+            value: '2',
+          },
+          {
+            type: NodeTypes.NUMBER,
+            value: '4',
+          },
+        ],
+      },
+    ],
+  }
+
+  expect(parser(tokens)).toEqual(ast)
+})
+
+test('two callExpression', () => {
+  const tokens = [
+    { type: TokenTypes.PAREN, value: '(' },
+    { type: TokenTypes.NAME, value: 'add' },
+    { type: TokenTypes.NUMBER, value: '2' },
+    { type: TokenTypes.NUMBER, value: '4' },
+    { type: TokenTypes.PAREN, value: ')' },
+    { type: TokenTypes.PAREN, value: '(' },
+    { type: TokenTypes.NAME, value: 'add' },
+    { type: TokenTypes.NUMBER, value: '3' },
+    { type: TokenTypes.NUMBER, value: '5' },
+    { type: TokenTypes.PAREN, value: ')' },
+  ]
+
+  const ast = {
+    type: NodeTypes.ROOT,
+    body: [
+      {
+        type: NodeTypes.CALL_EXPRESSION,
+        name: 'add',
+        params: [
+          {
+            type: NodeTypes.NUMBER,
+            value: '2',
+          },
+          {
+            type: NodeTypes.NUMBER,
+            value: '4',
+          },
+        ],
+      },
+      {
+        type: NodeTypes.CALL_EXPRESSION,
+        name: 'add',
+        params: [
+          {
+            type: NodeTypes.NUMBER,
+            value: '3',
+          },
+          {
+            type: NodeTypes.NUMBER,
+            value: '5',
+          },
+        ],
+      },
+    ],
+  }
+
+  expect(parser(tokens)).toEqual(ast)
+})
+
+test('number', () => {
+  const tokens = [
+    {
+      type: TokenTypes.NUMBER,
+      value: '2',
+    },
+  ]
+
+  const ast = {
+    type: NodeTypes.ROOT,
+    body: [
+      {
+        type: NodeTypes.NUMBER,
+        value: '2',
       },
     ],
   }
--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,84 @@
-import type { Token } from './tokenizer'
+import { type Token, TokenTypes } from './tokenizer'
+
+export enum NodeTypes {
+  ROOT,
+  NUMBER,
+  CALL_EXPRESSION,
+}
+
+type ChildNode = NumberNode | CallExpressionNode
+
+interface Node {
+  type: NodeTypes
+}
+
+interface RootNode extends Node {
+  body: ChildNode[]
+}
+
+interface NumberNode extends Node {
+  value: string
+}
+
+interface CallExpressionNode extends Node {
+  name: string
+  params: NumberNode[]
+}
+
+function createRootNode(): RootNode {
+  return {
+    type: NodeTypes.ROOT,
+    body: [],
+  }
+}
+
+function createNumberNode(value: string): NumberNode {
+  return {
+    type: NodeTypes.NUMBER,
+    value,
+  }
+}
+
+function createCallExpressionNode(name: string): CallExpressionNode {
+  return {
+    type: NodeTypes.CALL_EXPRESSION,
+    name,
+    params: [],
+  }
+}
 
 export function parser(tokens: Token[]) {
+  let current = 0
+  const rootNode = createRootNode()
 
+  function walk() {
+    let token = tokens[current]
+
+    if (token.type === TokenTypes.NUMBER) {
+      current++
+      return createNumberNode(token.value)
+    }
+
+    if (token.type === TokenTypes.PAREN && token.value === '(') {
+      token = tokens[++current]
+      const node = createCallExpressionNode(token.value)
+
+      token = tokens[++current]
+      while (!(token.type === TokenTypes.PAREN && token.value === ')')) {
+        // @ts-expect-error
+        node.params.push(walk())
+        token = tokens[current]
+      }
+
+      current++
+      return node
+    }
+
+    throw new Error(`不认识的token: ${token}`)
+  }
+
+  while (current < tokens.length)
+    rootNode.body.push(walk())
+
+  return rootNode
 }
